4ff1854edda717bc7c4a904465cfbd93b691753b
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-02 18:58:44 -0300

    testando e conhecendo python

--- primeiro_passo.py
+++ primeiro_passo.py
@@ -1,1 +1,11 @@
 print("Primeiros passos com Python")
+print("Diferente de outras linguagens como Java, o python tem sua tipagem fraca e dinâmica. Ou seja, não precisa declarar o tipo de variável que está sendo utilizada, por exemplo.")
+
+print("Exemplos:")
+
+a=10
+b="string usando aspas"
+c='f'
+d=50.3
+
+print(a," ",b," ",c," ",d)
