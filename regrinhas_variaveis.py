30f762d5d4681ad36dccba2a033efc831fb023f0
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-02 19:11:06 -0300

    sobre regrinhas de variáveis

---
+++ regrinhas_variaveis.py
@@ -0,0 +1,9 @@
+print("1a regra: Não pode ter espaços")
+print("2a regra: Não pode ter caracter especial")
+print("3a regra: Não pode começar com numeros")
+print("Utilize CamelCase")
+# comentario
+# comentario
+# comentario
+# comentario
+# Basta selecionar tudo e 'CTRL + ;'
