e63239eec4396fb1e716a0fbcad70dbbc4cd8d5c
PedroSimbera <pedrosimbera@hotmail.com>
2023-05-02 19:03:30 -0300

    sobre constantes

---
+++ constantes.py
@@ -0,0 +1,3 @@
+print("Constante, diferente de variáveis, não muda.")
+print("Entretanto, para o python não se aplica bem assim. Nativamente não tem tipos constantes, pode até se conseguir deixar como constantes, mas a linguagem aceita alteração")
+print ("Basta definir a variável como MAIUSCULO")
